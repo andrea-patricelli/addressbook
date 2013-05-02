403b1b33d2c4a2a6a7ba1adb3a1526dc4ad00199
Andrea Patricelli <andrea.patricelli@tirasa.net>
2013-05-02 17:38:24 +0200

    added applicationContextDispatcher-servlet.xml file to use @Autowired with DataSource configuration, added a new constructor for DatabaseException fixes #5, fixes #6

--- src/main/java/net/tirasa/test/addressbook/dao/impl/PersonDAOJdbcImpl.java
+++ src/main/java/net/tirasa/test/addressbook/dao/impl/PersonDAOJdbcImpl.java
@@ -3,66 +3,81 @@ package net.tirasa.test.addressbook.dao.impl;
 import java.util.List;
 import net.tirasa.test.addressbook.dao.PersonDAO;
 import net.tirasa.test.addressbook.data.Person;
+import net.tirasa.test.addressbook.exceptions.DatabaseException;
 import net.tirasa.test.addressbook.mapper.PersonMapper;
 import org.apache.commons.dbcp.BasicDataSource;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class PersonDAOJdbcImpl implements PersonDAO {
-//    @Autowired
-    private BasicDataSource dataSource;
     
     @Autowired
-    public void setDataSource(BasicDataSource dataSource) {
-        this.dataSource = dataSource;
-    }
+    private BasicDataSource dataSource;
 
     @Override
-    public void save(String id, String name, String email, String telephone){
+    public void save(String id, String name, String email, String telephone) throws DatabaseException {
         JdbcTemplate jdbcTemplateObject = new JdbcTemplate(this.dataSource);
         Person searched = this.find(id); // if it doesn't work use name instead of id
         StringBuilder SQL = new StringBuilder();
-        if (searched != null) {
-            SQL.append("UPDATE Persons SET Name='").append(name).append("', Email='").append(email).append(
-                    "', Telephone='").append(telephone).append("' WHERE Name='").append(id).append('\'');
-            jdbcTemplateObject.update(SQL.toString());
-        } else {
-            SQL.append("INSERT INTO Persons (Name, Email, Telephone) VALUES ('").append(name).append("', '").
-                    append(email).append("', '").append(telephone).append("')");
-            jdbcTemplateObject.update(SQL.toString());
+        try {
+            if (searched != null) {
+                SQL.append("UPDATE Persons SET Name='").append(name).append("', Email='").append(email).append(
+                        "', Telephone='").append(telephone).append("' WHERE Name='").append(id).append('\'');
+                jdbcTemplateObject.update(SQL.toString());
+            } else {
+                SQL.append("INSERT INTO Persons (Name, Email, Telephone) VALUES ('").append(name).append("', '").
+                        append(email).append("', '").append(telephone).append("')");
+                jdbcTemplateObject.update(SQL.toString());
+            }
+        } catch (DataAccessException e) {
+            throw new DatabaseException(e);
         }
     }
 
     @Override
-    public Person find(String requestParam_id) {
+    public Person find(String requestParam_id) throws DatabaseException {
         JdbcTemplate jdbcTemplateObject = new JdbcTemplate(this.dataSource);
         StringBuilder SQL = new StringBuilder();
         SQL.append("SELECT * FROM Persons WHERE Name='").append(requestParam_id).append('\'');
-        List<Person> searchedPersons = jdbcTemplateObject.query(SQL.toString(), new PersonMapper());
         Person searched = null;
-        // check if returned list is empty, if not get the first (and only) element of Person type returned
-        if (!searchedPersons.isEmpty()) {
-            searched = searchedPersons.iterator().next();
+        try {
+            List<Person> searchedPersons = jdbcTemplateObject.query(SQL.toString(), new PersonMapper());
+            // check if returned list is empty, if not get the first (and only) element of Person type returned
+            if (!searchedPersons.isEmpty()) {
+                searched = searchedPersons.iterator().next();
+            }
+        } catch (DataAccessException e) {
+            throw new DatabaseException(e);
         }
         return searched;
     }
 
     @Override
-    public List<Person> list() {
+    public List<Person> list() throws DatabaseException {
         JdbcTemplate jdbcTemplateObject = new JdbcTemplate(this.dataSource);
         List<Person> collection;
         String SQL = "SELECT * FROM Persons";
-        collection = jdbcTemplateObject.query(SQL, new PersonMapper());
+        try {
+            collection = jdbcTemplateObject.query(SQL, new PersonMapper());
+        } catch (DataAccessException e) {
+            throw new DatabaseException(e);
+        }
         return collection;
     }
 
     @Override
-    public void delete(String requestParam_id) {
+    public void delete(String requestParam_id) throws DatabaseException {
         JdbcTemplate jdbcTemplateObject = new JdbcTemplate(this.dataSource);
         StringBuilder SQL = new StringBuilder();
-        jdbcTemplateObject.update(SQL.append("DELETE FROM Persons WHERE Name ='").append(requestParam_id).append('\'').
-                toString());
+        try {
+            jdbcTemplateObject.update(SQL.append("DELETE FROM Persons WHERE Name ='").append(requestParam_id).append(
+                    '\'').
+                    toString());
+        } catch (DataAccessException e) {
+            throw new DatabaseException(e);
+        }
     }
 }
--- src/main/java/net/tirasa/test/addressbook/exceptions/DatabaseException.java
+++ src/main/java/net/tirasa/test/addressbook/exceptions/DatabaseException.java
@@ -1,11 +1,21 @@
 package net.tirasa.test.addressbook.exceptions;
 
+import java.util.logging.Logger;
+import net.tirasa.test.addressbook.dao.impl.PersonDAOJdbcImpl;
+import org.slf4j.LoggerFactory;
+
 public class DatabaseException extends Exception {
+
+    private final static Logger logger = (Logger) LoggerFactory.getLogger(PersonDAOJdbcImpl.class);
 
     private static final long serialVersionUID = -7577600787915618669L;
 
     public DatabaseException(String message) {
         super(message);
-        System.err.println(message);
+    }
+
+    public DatabaseException(Throwable cause) {
+        super(cause);
+        logger.info(cause.getMessage());
     }
 }
